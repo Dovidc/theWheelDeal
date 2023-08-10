6e636ad552add60599da6374c8cf48df0aa05925
Sayfullah Mujaahid <sayoof1050@hotmail.com>
2023-08-10 14:18:19 -0400

    Added unfinished employee dashboard and unfinished profile pages

---
+++ vue/src/views/Employee.vue
@@ -0,0 +1,17 @@
+<template>
+   <employee-dashboard />
+</template>
+
+<script>
+import EmployeeDashboard from '../components/EmployeeDashboard'
+
+export default {
+components: {
+    EmployeeDashboard
+}
+};
+</script>
+
+<style >
+
+</style>
