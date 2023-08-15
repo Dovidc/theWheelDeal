86d7fc99cd422bd942cba943d99316ecf969989b
Brian Ellis <b.matthew.ellis@gmail.com>
2023-08-15 14:57:47 -0400

    finished controllers

--- java/src/main/java/com/techelevator/controller/InvoiceController.java
+++ java/src/main/java/com/techelevator/controller/InvoiceController.java
@@ -2,16 +2,16 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.InvoiceDao;
 import com.techelevator.dao.UserDao;
+import com.techelevator.dao.WorkOrderDao;
 import com.techelevator.model.Invoice;
+import com.techelevator.model.InvoiceDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
 
@@ -23,6 +23,8 @@ public class InvoiceController {
     private InvoiceDao invoiceDao;
     @Autowired
     private UserDao userDao;
+    @Autowired
+    private WorkOrderDao workOrderDao;
 
     @RequestMapping(path = "/profile/{userId}/invoices", method = RequestMethod.GET)
     public List<Invoice> getInvoicesByUserId (Principal principal, @PathVariable int userId) {
@@ -35,5 +37,13 @@ public class InvoiceController {
 
 
     @RequestMapping(path = "/workorders/{workOrderId}/createinvoice", method = RequestMethod.POST)
-    public Invoice createInvoice (
+    public Invoice createInvoice (Principal principal, @PathVariable int workOrderId,
+                                  @Valid @RequestBody InvoiceDTO invoiceDTO) {
+        if (userDao.getUserByUsername(principal.getName()).getRole().equalsIgnoreCase("customer")) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Customer cannot create invoice.");
+        } else {
+            invoiceDTO.setWorkOrder(workOrderDao.getWorkOrderById(workOrderId));
+            return invoiceDao.createInvoice(invoiceDTO);
+        }
+    }
 }
